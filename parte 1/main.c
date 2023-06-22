f683538e6403d72607e6ae500ae876fbb0826f0f
fiorotticaio <caiofiorotti@gmail.com>
2023-06-22 17:36:15 -0300

    :tada: first commit

---
+++ parte 1/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Problema dos macacos\n");
+  return 0;
+}
---
+++ parte 2/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Implementando monitores\n");
+  return 0;
+}
